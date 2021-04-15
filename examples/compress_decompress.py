3d35e6b3ad4d8bce699b8edf7e5db5efb27e0b10
martius <martaiborra24@gmail.com>
2021-04-15 09:26:38 +0200

    Update c-blosc2 submodule

--- examples/compress_decompress.py
+++ examples/compress_decompress.py
@@ -2,7 +2,7 @@
 import blosc2
 import array
 
-a = array.array('i', range(1000*1000))
+a = array.array('i', range(1000 * 1000))
 a_bytesobj = a.tobytes()
 c_bytesobj = blosc2.compress(a_bytesobj, typesize=4)
 assert len(c_bytesobj) < len(a_bytesobj)
@@ -10,5 +10,5 @@ a_bytesobj2 = blosc2.decompress(c_bytesobj)
 assert a_bytesobj == a_bytesobj2
 
 dest = blosc2.compress(b"", 1)
-assert(b""==blosc2.decompress(dest))
-assert(type(blosc2.decompress(blosc2.compress(b"1"*7, 8),as_bytearray=True)) is bytearray)
+assert(b"" == blosc2.decompress(dest))
+assert(type(blosc2.decompress(blosc2.compress(b"1"*7, 8), as_bytearray=True)) is bytearray)
