76f847ecd902ce529ffafbc35f82adfbe78458c0
martius <martaiborra24@gmail.com>
2021-04-21 12:50:07 +0200

    Add blosc logo

--- doc/conf.py
+++ doc/conf.py
@@ -47,7 +47,13 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+html_static_path = ["_static"]
 html_theme = "pydata_sphinx_theme"
+html_logo = "_static/blosc-logo_128.png"
+html_favicon = "_static/blosc-logo_128.png"
+html_show_sourcelink = False
+
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
