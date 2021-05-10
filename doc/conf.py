5e076303c91a1c5f686bcb2185fcbb2dcaa461b0
Aleix Alcacer <aleixalcacer@gmail.com>
2021-05-10 09:53:31 +0200

    Add release notes in docs

--- doc/conf.py
+++ doc/conf.py
@@ -31,7 +31,10 @@ author = "The Blosc Development Team"
 extensions = [
     "sphinx.ext.autodoc",
     "numpydoc",
+    "myst_parser"
 ]
+
+source_suffix = ['.rst', '.md']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
