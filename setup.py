d30b9d7c3c73fcc4a39e6a5688e08d3377b97549
Francesc Alted <francesc@blosc.org>
2021-05-15 12:37:58 +0200

    No need to specify cmake args anymore

--- setup.py
+++ setup.py
@@ -55,19 +55,6 @@ setup(
     license="https://opensource.org/licenses/BSD-3-Clause",
     platforms=["any"],
     setup_requires=["cython>=0.29", "scikit-build", "cmake>=3.14.0"],
-    cmake_args=[
-        "-DDEACTIVATE_SSE2:BOOL=%s"
-        % cmake_bool(
-            ("DISABLE_BLOSC_SSE2" in os.environ) or (cpu_info is None) or ("sse2" not in cpu_info["flags"])
-        ),
-        "-DDEACTIVATE_AVX2:BOOL=%s"
-        % cmake_bool(
-            ("DISABLE_BLOSC_AVX2" in os.environ) or (cpu_info is None) or ("avx2" not in cpu_info["flags"])
-        ),
-        "-DDEACTIVATE_LZ4:BOOL=%s" % cmake_bool(not int(os.environ.get("INCLUDE_LZ4", "1"))),
-        "-DDEACTIVATE_ZLIB:BOOL=%s" % cmake_bool(not int(os.environ.get("INCLUDE_ZLIB", "1"))),
-        "-DDEACTIVATE_ZSTD:BOOL=%s" % cmake_bool(not int(os.environ.get("INCLUDE_ZSTD", "1"))),
-    ],
     tests_require=["numpy", "psutil"],
     packages=["blosc2"],
     package_dir={"blosc2": "blosc2"},
