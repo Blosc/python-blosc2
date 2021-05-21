744f9cc2f6d0111f60a2fb5a84665cf82f39fe27
Francesc Alted <francesc@blosc.org>
2021-05-21 15:49:35 +0200

    Repeat measurements and do a mean

--- bench/pack_compress.py
+++ bench/pack_compress.py
@@ -19,6 +19,7 @@ import numpy as np
 
 import blosc2
 
+NREP = 4
 N = int(1e8)
 clevel = 5
 Nexp = np.log10(N)
@@ -26,22 +27,24 @@ Nexp = np.log10(N)
 blosc2.print_versions()
 print("Creating a large NumPy array with 10**%d int64 elements:" % Nexp)
 # in_ = np.arange(N, dtype=np.int64)  # the trivial linear distribution
-# in_ = np.linspace(0, 1000, N)  # another linear distribution
-in_ = np.random.randint(0, 10_000, N)  # random distribution
+in_ = np.linspace(0, 10_000, N)  # another linear distribution
+# in_ = np.random.randint(0, 10_000, N)  # random distribution
 print(" ", in_)
 
 tic = time.time()
-out_ = np.copy(in_)
+for i in range(NREP):
+    out_ = np.copy(in_)
 toc = time.time()
-tcpy = toc - tic
+tcpy = (toc - tic) / NREP
 print("  Time for copying array with np.copy:\t\t\t%.3f s (%.2f GB/s))" %
       (tcpy, ((N * 8 / tcpy) / 2 ** 30)))
 
 out_ = np.empty_like(in_)
 tic = time.time()
-np.copyto(out_, in_)
+for i in range(NREP):
+    np.copyto(out_, in_)
 toc = time.time()
-tcpy = toc - tic
+tcpy = (toc - tic) / NREP
 print("  Time for copying array with np.copyto and empty_like:\t%.3f s (%.2f GB/s))" %
       (tcpy, ((N * 8 / tcpy) / 2 ** 30)))
 
@@ -51,57 +54,64 @@ print("  Time for copying array with np.copyto and empty_like:\t%.3f s (%.2f GB/
 # Here we benchmark what happens when we allocate memory using calloc
 out_ = np.zeros(in_.shape, dtype=in_.dtype)
 tic = time.time()
-np.copyto(out_, in_)
+for i in range(NREP):
+    np.copyto(out_, in_)
 toc = time.time()
-tcpy = toc - tic
+tcpy = (toc - tic) / NREP
 print("  Time for copying array with np.copyto and zeros:\t%.3f s (%.2f GB/s))" %
       (tcpy, ((N * 8 / tcpy) / 2 ** 30)))
 
 # Cause a page fault before the benchmark
 out_ = np.full_like(in_, fill_value=0)
 tic = time.time()
-np.copyto(out_, in_)
+for i in range(NREP):
+    np.copyto(out_, in_)
 toc = time.time()
-tcpy = toc - tic
+tcpy = (toc - tic) / NREP
 print("  Time for copying array with np.copyto and full_like:\t%.3f s (%.2f GB/s))" %
       (tcpy, ((N * 8 / tcpy) / 2 ** 30)))
 
 out_ = np.full_like(in_, fill_value=0)
 tic = time.time()
-out_[...] = in_
+for i in range(NREP):
+    out_[...] = in_
 toc = time.time()
-tcpy = toc - tic
+tcpy = (toc - tic) / NREP
 print("  Time for copying array with numpy assignment:\t\t%.3f s (%.2f GB/s))" %
       (tcpy, ((N * 8 / tcpy) / 2 ** 30)))
 print()
 
 for cname in blosc2.compressor_list():
-    print("Using *** %s *** compressor::" % cname)
+    print("Using *** %s *** compressor:" % cname)
     ctic = time.time()
-    c = blosc2.pack_array(in_, clevel=clevel, shuffle=True, cname=cname)
+    for i in range(NREP):
+        c = blosc2.pack_array(in_, clevel=clevel, shuffle=True, cname=cname)
     ctoc = time.time()
     dtic = time.time()
-    out = blosc2.unpack_array(c)
+    for i in range(NREP):
+        out = blosc2.unpack_array(c)
     dtoc = time.time()
 
     assert np.array_equal(in_, out)
-    tc = ctoc - ctic
-    td = dtoc - dtic
+    tc = (ctoc - ctic) / NREP
+    td = (dtoc - dtic) / NREP
     print("  Time for pack_array/unpack_array:     %.3f/%.3f s (%.2f/%.2f GB/s))." %
           (tc, td, ((N * 8 / tc) / 2 ** 30), ((N * 8 / td) / 2 ** 30)), end="")
     print("\tCompr ratio: %.2f" % (in_.size * in_.dtype.itemsize * 1.0 / len(c)))
 
     ctic = time.time()
-    c = blosc2.compress(in_, clevel=clevel, shuffle=True, cname=cname)
+    for i in range(NREP):
+        c = blosc2.compress(in_, clevel=clevel, shuffle=True, cname=cname)
     ctoc = time.time()
     out = np.full_like(in_, fill_value=0)
     dtic = time.time()
-    blosc2.decompress(c, dst=out)
+    for i in range(NREP):
+        blosc2.decompress(c, dst=out)
     dtoc = time.time()
 
     assert np.array_equal(in_, out)
-    tc = ctoc - ctic
-    td = dtoc - dtic
+    tc = (ctoc - ctic) / NREP
+    td = (dtoc - dtic) / NREP
     print("  Time for compress/decompress:         %.3f/%.3f s (%.2f/%.2f GB/s))." %
           (tc, td, ((N * 8 / tc) / 2 ** 30), ((N * 8 / td) / 2 ** 30)), end="")
     print("\tCompr ratio: %.2f" % (in_.size * in_.dtype.itemsize * 1.0 / len(c)))
