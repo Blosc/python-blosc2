166ec5a85d73350c9d1a7dd39610584c7690d99c
Aleix Alcacer <aleixalcacer@gmail.com>
2021-07-21 13:03:34 +0200

    Fix Blosc logo

--- doc/conf.py
+++ doc/conf.py
@@ -52,8 +52,8 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 #
 html_static_path = ["_static"]
 html_theme = "pydata_sphinx_theme"
-html_logo = "_static/blosc-logo_128.png"
-html_favicon = "_static/blosc-logo_128.png"
+html_logo = "blosc-logo_128.png"
+html_favicon = "blosc-logo_128.png"
 html_show_sourcelink = False
 
 
