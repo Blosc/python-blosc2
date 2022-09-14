a848be3afd7626bb75e9a10e98fdc59e743c3ded
Marta Iborra <martaiborra24@gmail.com>
2022-09-14 12:15:35 +0200

    Add get_slice bench

---
+++ bench/get_slice.py
@@ -0,0 +1,55 @@
+#######################################################################
+# Copyright (C) 2019-present, Blosc Development team <blosc@blosc.org>
+# All rights reserved.
+#
+# This source code is licensed under a BSD-style license (found in the
+# LICENSE file in the root directory of this source tree)
+#######################################################################
+
+import blosc2
+import numpy as np
+import sys
+from time import time
+
+
+# Dimensions, type and persistence properties for the arrays
+shape = 10_000 * 10_000
+chunksize = 100_000
+blocksize = 10_000
+
+dtype = np.float64
+
+nchunks = shape // chunksize
+# Set the compression and decompression parameters
+cparams = {"compcode": blosc2.Codec.BLOSCLZ, "typesize": 8, "blocksize": blocksize * 8}
+dparams = {}
+contiguous = True
+persistent = bool(sys.argv[1]) if len(sys.argv) > 1 else False
+
+if persistent:
+    urlpath = "bench_getitem.b2frame"
+else:
+    urlpath = None
+
+storage = {"contiguous": contiguous, "urlpath": urlpath, "cparams": cparams, "dparams": dparams}
+blosc2.remove_urlpath(urlpath)
+
+# Create the empty SChunk
+schunk = blosc2.SChunk(chunksize=chunksize * cparams["typesize"], **storage)
+
+# Append some chunks
+for i in range(nchunks):
+    buffer = i * np.arange(chunksize, dtype=dtype)
+    nchunks_ = schunk.append_data(buffer)
+    assert nchunks_ == (i + 1)
+
+
+# Use get_slice for reading blocks individually
+t0 = time()
+for i in range(shape // blocksize):
+    _ = schunk.get_slice(start=i * blocksize, stop=(i+1) * blocksize - 1)
+t1 = time()
+print("Time for reading with getitem: %.3fs" % (t1 - t0))
+
+blosc2.remove_urlpath(urlpath)
+
