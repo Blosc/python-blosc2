c94c390ae7830d87090d8fb954cc50a6af1e0da1
Marta Iborra <martaiborra24@gmail.com>
2023-02-08 10:39:37 +0100

    Add resize

---
+++ examples/ndarray/ex_resize.py
@@ -0,0 +1,28 @@
+#######################################################################
+# Copyright (C) 2019-present, Blosc Development team <blosc@blosc.org>
+# All rights reserved.
+#
+# This source code is licensed under a BSD-style license (found in the
+# LICENSE file in the root directory of this source tree)
+#######################################################################
+
+import blosc2
+import numpy as np
+
+np.random.seed(123)
+
+shape = (8, 8)
+chunks = (4, 4)
+blocks = (2, 2)
+cparams = {"nthreads": 2}
+dparams = {"nthreads": 2}
+
+
+fill_value = b"1"
+a = blosc2.full(shape, fill_value=fill_value, chunks=chunks, blocks=blocks,
+                cparams=cparams, dparams=dparams)
+print(a.schunk.cparams)
+print(a.schunk.dparams)
+a.resize((10, 10))
+
+print(a[:])
