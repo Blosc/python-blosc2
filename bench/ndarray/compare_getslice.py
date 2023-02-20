3345068e612b416c5a9d9752360bc52db3083ba3
Francesc Alted <francesc@blosc.org>
2023-02-20 14:28:54 +0100

    Measure sequential read speed too

--- bench/ndarray/compare_getslice.py
+++ bench/ndarray/compare_getslice.py
@@ -11,9 +11,10 @@
 # blosc2, Zarr and HDF5
 # In brief, each approach has its own strengths and weaknesses.
 #
-# Usage: pass any argument for testing the persistent backends.
-# Else, only in-memory containers will be tested.
+# Usage: pass any argument for testing the in-memory backends.
+# Else, only persistent containers will be tested.
 
+import math
 import os
 import shutil
 import sys
@@ -27,14 +28,13 @@ import blosc2
 import numpy as np
 
 
-persistent = bool(sys.argv[1]) if len(sys.argv) > 1 else False
+persistent = True if len(sys.argv) == 1 else False
 if persistent:
-    print("Testing the persistent backends...")
+    print("Testing the persistent backends")
 else:
-    print("Testing the in-memory backends...")
+    print("Testing the in-memory backends")
 
 # Dimensions and type properties for the arrays
-
 # 3D
 # shape = (1000, 2000, 250)
 # chunks = (50, 500, 50)
@@ -46,16 +46,17 @@ chunks = (10, 25,  50,  50)
 blocks = ( 3,  5,  10,  20)
 
 # Smaller sizes (for quick testing)
-#shape = (100, 200, 250)
-#chunks = (50, 50, 50)
-#blocks = (10, 10, 25)
+# shape = (100, 200, 250)
+# chunks = (50, 50, 50)
+# blocks = (10, 10, 25)
 
 # shape = (50, 100, 30, 25)
 # chunks = (10, 25, 20,  5)
 # blocks = ( 3,  5, 10,  2)
 
-dtype = np.float64
+dtype = np.dtype(np.float64)
 
+dset_size = math.prod(shape) * dtype.itemsize
 # Compression properties
 clevel = 1
 cname = "zstd"
@@ -82,31 +83,31 @@ if persistent:
     fname_h5py = "compare_getslice_h5py.h5"
     blosc2.remove_urlpath(fname_h5py)
 
-# Create content for populating arrays
+# Create datasets in different formats
 # content = np.random.normal(0, 1, int(np.prod(shape))).reshape(shape)
 content = np.linspace(0, 1, int(np.prod(shape))).reshape(shape)
 
+print("\nCreating datasets...")
 # Create and fill a NDArray
 t0 = time()
 b2 = blosc2.empty(shape, dtype=content.dtype, chunks=chunks, blocks=blocks,
                   urlpath=fname_b2nd, cparams=cparams)
 b2[:] = content
-acratio = b2.schunk.cratio
-t1 = time()
-print("Time for filling array (blosc2): %.3fs ; CRatio: %.1fx" % ((t1 - t0), acratio))
+t = time() - t0
+speed = dset_size / (t * 2**30)
+cratio = b2.schunk.cratio
+print(f"Time for filling array (blosc2): {t:.3f} s ({speed:.2f} GB/s) ; cratio: {cratio:.1f}x")
 
 # Create and fill a zarr array
 t0 = time()
 compressor = numcodecs.Blosc(cname=cname, clevel=clevel, shuffle=zfilter, blocksize=blocksize)
 numcodecs.blosc.set_nthreads(nthreads)
-if persistent:
-    z = zarr.open(fname_zarr, mode='w', shape=shape, chunks=chunks, dtype=dtype, compressor=compressor)
-else:
-    z = zarr.empty(shape=shape, chunks=chunks, dtype=dtype, compressor=compressor)
+z = zarr.open(fname_zarr, shape=shape, chunks=chunks, dtype=dtype, compressor=compressor)
 z[:] = content
-zratio = z.nbytes / z.nbytes_stored
-t1 = time()
-print("Time for filling array (zarr): %.3fs ; CRatio: %.1fx" % ((t1 - t0), zratio))
+t = time() - t0
+speed = dset_size / (t * 2**30)
+cratio = dset_size / z.nbytes_stored
+print(f"Time for filling array (zarr): {t:.3f} s ({speed:.2f} GB/s) ; cratio: {cratio:.1f}x")
 
 # Create and fill an HDF5 array (PyTables)
 t0 = time()
@@ -117,9 +118,10 @@ if persistent:
 else:
     h5f = tables.open_file(fname_tables, 'w', driver='H5FD_CORE', driver_core_backing_store=0)
 h5ca = h5f.create_carray(h5f.root, 'carray', filters=filters, chunkshape=chunks, obj=content)
-h5ratio = h5ca.size_in_memory / h5ca.size_on_disk
-t1 = time()
-print("Time for filling array (hdf5, tables): %.3fs ; CRatio: %.1fx" % ((t1 - t0), h5ratio))
+t = time() - t0
+speed = dset_size / (t * 2**30)
+cratio = dset_size / h5ca.size_on_disk
+print(f"Time for filling array (hdf5, tables): {t:.3f} s ({speed:.2f} GB/s) ; cratio: {cratio:.1f}x")
 
 # Create and fill an HDF5 array (h5py)
 t0 = time()
@@ -129,18 +131,46 @@ if persistent:
 else:
     h5pyf = h5py.File(fname_h5py, 'w', driver='core', backing_store=False)
 h5d = h5pyf.create_dataset('dataset', dtype=dtype, data=content, chunks=chunks, **filters)
-t1 = time()
+t = time() - t0
+speed = dset_size / (t * 2 ** 30)
 if persistent:
     num_blocks = os.stat(fname_h5py).st_blocks
-    block_size = os.statvfs(fname_h5py).f_bsize
+    # block_size = os.statvfs(fname_h5py).f_bsize
     size_on_disk = num_blocks * 512
-    h5ratio = h5d.nbytes / size_on_disk
-    print("Time for filling array (hdf5, h5py): %.3fs ; CRatio: %.1fx" % ((t1 - t0), h5ratio))
+    cratio = dset_size / size_on_disk
+    print(f"Time for filling array (hdf5, h5py): {t:.3f} s ({speed:.2f} GB/s) ; cratio: {cratio:.1f}x")
 else:
-    print("Time for filling array (hdf5, h5py): %.3fs ; CRatio: Not avail" % ((t1 - t0)))
+    print(f"Time for filling array (hdf5, h5py): {t:.3f} s ({speed:.2f} GB/s) ; cratio: Not avail")
 
+# Complete reads
+print("\nComplete reads...")
+t0 = time()
+r = b2[:]
+t = time() - t0
+speed = dset_size / (t * 2 ** 30)
+print(f"Time for complete read (blosc2): {t:.3f} s ({speed:.2f} GB/s)")
 
-# Setup the coordinates for random planes
+t0 = time()
+r = z[:]
+t = time() - t0
+speed = dset_size / (t * 2 ** 30)
+print(f"Time for complete read (zarr): {t:.3f} s ({speed:.2f} GB/s)")
+
+t0 = time()
+r = h5ca[:]
+t = time() - t0
+speed = dset_size / (t * 2 ** 30)
+print(f"Time for complete read (hdf5, tables): {t:.3f} s ({speed:.2f} GB/s)")
+
+t0 = time()
+r = h5d[:]
+t = time() - t0
+speed = dset_size / (t * 2 ** 30)
+print(f"Time for complete read (hdf5, h5py): {t:.3f} s ({speed:.2f} GB/s)")
+
+# Reading by slices
+print("\nReading by slices...")
+# The coordinates for random planes
 planes_idx = np.random.randint(0, min(shape), 100)
 
 def time_slices(dset, idx):
@@ -156,8 +186,9 @@ def time_slices(dset, idx):
         else:
             for i in idx:
                 r = dset[:, :, i]
+        t = time() - t0
         size = r.size * dset.dtype.itemsize * len(idx)
-        return time() - t0, size
+        return t, size / (t * 2**30)
     elif dset.ndim == 4:
         t0 = time()
         if ndim == 0:
@@ -172,8 +203,9 @@ def time_slices(dset, idx):
         else:
             for i in idx:
                 r = dset[:, :, :, i]
+        t = time() - t0
         size = r.size * dset.dtype.itemsize * len(idx)
-        return time() - t0, size
+        return t, size / (t * 2**30)
     raise ValueError(f"ndim == {dset.ndim} is not supported")
 
 
@@ -181,22 +213,20 @@ for ndim in range(len(shape)):
     print(f"Slicing in dim {ndim}...")
 
     # Slicing with blosc2
-    t, size = time_slices(b2, planes_idx)
-    print(f"Time for reading with getitem (blosc2): {t:.3f} s ({size / (t * 2**20):.2f} MB/s)")
+    t, speed = time_slices(b2, planes_idx)
+    print(f"Time for reading with getitem (blosc2): {t:.3f} s ({speed:.2f} GB/s)")
 
     # Slicing with zarr
-    t, size = time_slices(z, planes_idx)
-    print(f"Time for reading with getitem (zarr): {t:.3f} s ({size / (t * 2**20):.2f} MB/s)")
+    t, speed = time_slices(z, planes_idx)
+    print(f"Time for reading with getitem (zarr): {t:.3f} s ({speed:.2f} GB/s)")
 
     # Slicing with hdf5 (PyTables)
-    t, size = time_slices(h5ca, planes_idx)
-    print(f"Time for reading with getitem (hdf5, tables): {t:.3f} s ({size / (t * 2**20):.2f} MB/s)")
+    t, speed = time_slices(h5ca, planes_idx)
+    print(f"Time for reading with getitem (hdf5, tables): {t:.3f} s ({speed:.2f} GB/s)")
 
     # Slicing with hdf5 (h5py)
-    t, size = time_slices(h5d, planes_idx)
-    print(f"Time for reading with getitem (hdf5, h5py): {t:.3f} s ({size / (t * 2**20):.2f} MB/s)")
-
-    print()
+    t, speed = time_slices(h5d, planes_idx)
+    print(f"Time for reading with getitem (hdf5, h5py): {t:.3f} s ({speed:.2f} GB/s)")
 
 h5f.close()
 h5pyf.close()
