7b5b507d0d2956ed41b0f65a3e169837d6562c53
Ben Greiner <code@bnavigator.de>
2023-05-11 12:53:00 +0200

    Move setup metadata to pyproject.toml

--- setup.py
+++ setup.py
@@ -6,9 +6,6 @@
 # LICENSE.txt file in the root directory of this source tree)
 #######################################################################
 
-from pathlib import Path
-from textwrap import dedent
-
 from skbuild import setup
 
 
@@ -21,46 +18,12 @@ VERSION = open("VERSION").read().strip()
 # Create the version.py file
 open("blosc2/version.py", "w").write('__version__ = "%s"\n' % VERSION)
 
-classifiers = dedent(
-    """\
-    Development Status :: 5 - Production/Stable
-    Intended Audience :: Developers
-    Intended Audience :: Information Technology
-    Intended Audience :: Science/Research
-    License :: OSI Approved :: BSD License
-    Programming Language :: Python
-    Topic :: Software Development :: Libraries :: Python Modules
-    Operating System :: Microsoft :: Windows
-    Operating System :: Unix
-    Programming Language :: Python :: 3
-    Programming Language :: Python :: 3.8
-    Programming Language :: Python :: 3.9
-    Programming Language :: Python :: 3.10
-    Programming Language :: Python :: 3.11
-    """
-)
-
-
-# read the contents of the README file
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.rst").read_text()
-
+# These keywords need to be in setup()
+# https://scikit-build.readthedocs.io/en/latest/usage.html#setuptools-options
 setup(
-    name="blosc2",
     version=VERSION,
-    description="Python wrapper for the C-Blosc2 library.",
-    long_description=long_description,
-    long_description_content_type="text/x-rst",
-    classifiers=[c for c in classifiers.split("\n") if c],
-    python_requires=">=3.8, <4",
-    author="Blosc Development Team",
-    author_email="blosc@blosc.org",
-    maintainer="Blosc Development Team",
-    maintainer_email="blosc@blosc.org",
-    url="https://github.com/Blosc/python-blosc2",
-    license="https://opensource.org/licenses/BSD-3-Clause",
-    platforms=["any"],
     packages=["blosc2"],
     package_dir={"blosc2": "blosc2"},
+    include_package_data=True,
     install_requires=open("requirements-runtime.txt").read().split(),
 )
