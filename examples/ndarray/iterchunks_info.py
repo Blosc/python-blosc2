4f2a290ed7a809d7d330a8a9dd0d8ed5c5a2c0d5
Francesc Alted <francesc@blosc.org>
2023-06-12 17:53:52 +0200

    Cap on non-special buffers

--- examples/ndarray/iterchunks_info.py
+++ examples/ndarray/iterchunks_info.py
@@ -15,14 +15,18 @@ import blosc2
 shape = (1_000,) * 3
 chunks = (500,) * 3
 dtype = "f4"
-a = blosc2.full(shape, fill_value=9, chunks=chunks, dtype=dtype)
+
+# Create the NDArray with a mix of different special values (and not special too!)
 # a = blosc2.zeros(shape, chunks=chunks, dtype=dtype)
-# print(a.info)
+a = blosc2.full(shape, fill_value=9, chunks=chunks, dtype=dtype)
+slice_ = (slice(0, 500), slice(0, 500), slice(0, 500))
+a[slice_] = 0  # introduce a zeroed chunk (another type of special value)
+slice_ = (slice(-500, -1), slice(-500, -1), slice(-500, -1))
+a[slice_] = 1  # blosc2 is currently not able to determine special values in this case
 
 # Iterate over chunks
 t0 = time()
 for info in a.iterchunks_info():
     print(info)
     # Do something with the chunk
-    pass
 print(f"Time for iterating over {a.schunk.nchunks} chunks: {time() - t0:.4f} s")
