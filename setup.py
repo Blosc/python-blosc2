1bb5923088646c470e41a283a743c8ee7c83a71d
Francesc Alted <francesc@blosc.org>
2023-09-26 19:26:52 +0200

    Migrated to use scikit-build-core

--- setup.py
+++ setup.py
@@ -9,26 +9,12 @@
 from skbuild import setup
 
 
-def cmake_bool(cond):
-    return "ON" if cond else "OFF"
-
-
-# Blosc version
-VERSION = open("VERSION").read().strip()
-# Create the version.py file
-open("blosc2/version.py", "w").write(f'__version__ = "{VERSION}"\n')
-
 def exclude_pkgconfig(cmake_manifest):
     """remove pkgconfig file from installation: gh-110."""
-    return list(filter(lambda name: not (name.endswith('.pc')), cmake_manifest))
+    return list(filter(lambda name: not (name.endswith(".pc")), cmake_manifest))
 
-# These keywords need to be in setup()
-# https://scikit-build.readthedocs.io/en/latest/usage.html#setuptools-options
+
 setup(
-    version=VERSION,
     packages=["blosc2"],
-    package_dir={"blosc2": "blosc2"},
-    include_package_data=True,
     cmake_process_manifest_hook=exclude_pkgconfig,
-    install_requires=open("requirements-runtime.txt").read().split(),
 )
