ca353151c0c3c2ff9563448a03ec3a12987becf6
Francesc Alted <francesc@blosc.org>
2024-03-10 09:54:00 +0100

    New test unit for lazyexpr

---
+++ tests/ndarray/test_lazyexpr.py
@@ -0,0 +1,87 @@
+#######################################################################
+# Copyright (c) 2019-present, Blosc Development Team <blosc@blosc.org>
+# All rights reserved.
+#
+# This source code is licensed under a BSD-style license (found in the
+# LICENSE file in the root directory of this source tree)
+#######################################################################
+
+import numexpr as ne
+import numpy as np
+import pytest
+
+import blosc2
+
+NITEMS = 1_000_000
+
+
+@pytest.fixture(params=[np.float32, np.float64])
+def array_fixture(request):
+    dtype = request.param
+    na1 = np.linspace(0, 10, NITEMS, dtype=dtype)
+    a1 = blosc2.asarray(na1)
+    na2 = np.copy(na1)
+    a2 = blosc2.asarray(na2)
+    na3 = np.copy(na1)
+    a3 = blosc2.asarray(na3)
+    na4 = np.copy(na1)
+    a4 = blosc2.asarray(na4)
+    return a1, a2, a3, a4, na1, na2, na3, na4
+
+
+def test_simple_getitem(array_fixture):
+    a1, a2, a3, a4, na1, na2, na3, na4 = array_fixture
+    expr = a1 + a2 - a3 * a4
+    nres = ne.evaluate("na1 + na2 - na3 * na4")
+    sl = slice(100)
+    res = expr[sl]
+    np.testing.assert_allclose(res, nres[sl])
+
+
+# Add more test functions to test different aspects of the code
+def test_simple_expression(array_fixture):
+    a1, a2, a3, a4, na1, na2, na3, na4 = array_fixture
+    expr = a1 + a2 - a3 * a4
+    nres = ne.evaluate("na1 + na2 - na3 * na4")
+    res = expr.evaluate()
+    np.testing.assert_allclose(res[:], nres)
+
+
+def test_complex_evaluate(array_fixture):
+    a1, a2, a3, a4, na1, na2, na3, na4 = array_fixture
+    expr = blosc2.tan(a1) * (blosc2.sin(a2) * blosc2.sin(a2) + blosc2.cos(a3)) + (blosc2.sqrt(a4) * 2)
+    expr += 2
+    nres = ne.evaluate("tan(na1) * (sin(na2) * sin(na2) + cos(na3)) + (sqrt(na4) * 2) + 2")
+    res = expr.evaluate()
+    np.testing.assert_allclose(res[:], nres)
+
+
+def test_complex_getitem(array_fixture):
+    a1, a2, a3, a4, na1, na2, na3, na4 = array_fixture
+    expr = blosc2.tan(a1) * (blosc2.sin(a2) * blosc2.sin(a2) + blosc2.cos(a3)) + (blosc2.sqrt(a4) * 2)
+    expr += 2
+    nres = ne.evaluate("tan(na1) * (sin(na2) * sin(na2) + cos(na3)) + (sqrt(na4) * 2) + 2")
+    res = expr[:]
+    np.testing.assert_allclose(res, nres)
+
+
+def test_complex_getitem_slice(array_fixture):
+    a1, a2, a3, a4, na1, na2, na3, na4 = array_fixture
+    expr = blosc2.tan(a1) * (blosc2.sin(a2) * blosc2.sin(a2) + blosc2.cos(a3)) + (blosc2.sqrt(a4) * 2)
+    expr += 2
+    nres = ne.evaluate("tan(na1) * (sin(na2) * sin(na2) + cos(na3)) + (sqrt(na4) * 2) + 2")
+    sl = slice(100)
+    res = expr[sl]
+    np.testing.assert_allclose(res, nres[sl])
+
+
+# TODO: this is failing
+def _test_expression_with_constants(array_fixture):
+    a1, a2, a3, a4, na1, na2, na3, na4 = array_fixture
+    expr = a1 + 2 - a3 * 3.14
+    nres = ne.evaluate("na1 + 2 - na3 * 3.14")
+    res = expr.evaluate()
+    np.testing.assert_allclose(res[:], nres)
+
+
+# TODO: extend this to more expressions, functions and types
