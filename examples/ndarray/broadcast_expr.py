c852b04712bc9085631e687baefc4135b1e9a325
Francesc Alted <francesc@blosc.org>
2024-05-20 08:13:14 +0200

    Updated example on broadcasting

--- examples/ndarray/broadcast_expr.py
+++ examples/ndarray/broadcast_expr.py
@@ -6,31 +6,34 @@
 # LICENSE file in the root directory of this source tree)
 #######################################################################
 
-# This shows how values in expressions support broadcast with NDArray/NumPy arrays as operands
+# This shows how to evaluate expressions with NDArray instances having different shapes as operands.
+# The broadcast is done internally and tries to mimic NumPy behavior.
 
 import numpy as np
 
 import blosc2
 
-shape = (2, 5)
+# Two operands with the next shapes should be supported
+# shape1, shape2 = (2, 1, 3, 2), (3, 3, 2)
+# shape1, shape2 = (2, 1, 3, 2), (3, 1, 2)
+shape1, shape2 = (2, 1, 1, 1), (3, 2, 2)
 
 # Create a NDArray from a NumPy array
-npa = np.linspace(0, 1, np.prod(shape), dtype=np.float32).reshape(shape)
-npb = np.linspace(1, 2, np.prod(shape), dtype=np.float64).reshape(shape)
-npc = npa + npb.mean(axis=0)
-# npres = npa + np.mean(npa, axis=0)
-npres = npa + np.mean(npc, axis=0)
+npa = np.linspace(0, 1, np.prod(shape1), dtype=np.float32).reshape(shape1)
+npb = np.linspace(1, 2, np.prod(shape2), dtype=np.float64).reshape(shape2)
+npc = npa + npb
+npres = npa + npb
 print("Broadcast with NumPy:\n", npres)
 
 a = blosc2.asarray(npa)
 b = blosc2.asarray(npb)
 
 # Get a LazyExpr instance
-c = a + b.mean(axis=0)
+c = a + b
 # Evaluate: output is a NDArray
 # d = a + blosc2.mean(a, axis=0)
 # d = a + np.mean(npa, axis=0)
-d = a + blosc2.mean(c, axis=0)
+d = a + b
 # print(d, d.shape, d.dtype)
 # print(d.expression, d.operands)
 assert isinstance(d, blosc2.LazyExpr)
