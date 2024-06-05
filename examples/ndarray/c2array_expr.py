7db8a40a99da6dc461a019de3ddbc5e7ab240e42
Francesc Alted <francesc@blosc.org>
2024-06-05 20:31:57 +0200

    Fix C2Array example

--- examples/ndarray/c2array_expr.py
+++ examples/ndarray/c2array_expr.py
@@ -1,21 +1,28 @@
-import blosc2
+import pathlib
+
 import numpy as np
 
-# host = 'demo-api.caterva2.net:8202'
-host = 'localhost:8002'
+import blosc2
+
+host = "https://demo.caterva2.net/"
+# host = 'localhost:8002'
 # root = 'example'
-root = 'foo'
 # name = 'dir1/ds-2d.b2nd'
-name1 = 'operands/ds-0-10-linspace-float32-(True, True)-a1-(1000,)d.b2nd'
-name2 = 'operands/ds-0-10-linspace-float32-(True, True)-a1-(1000,)d.b2nd'
-a = blosc2.C2Array(name1, root, host)
-b = blosc2.C2Array(name2, root, host)
-print(a[990:1000])
+
+root = "b2tests"
+dir = "expr/"
+name1 = "ds-0-10-linspace-float64-(True, True)-a1-(60, 60)d.b2nd"
+name2 = "ds-0-10-linspace-float64-(True, True)-a2-(60, 60)d.b2nd"
+path1 = pathlib.Path(f"{root}/{dir + name1}").as_posix()
+path2 = pathlib.Path(f"{root}/{dir + name2}").as_posix()
+
+a = blosc2.C2Array(path1, host)
+b = blosc2.C2Array(path2, host)
+print(a[10:20, 10:20])
 c = a + b
 print(type(c))
 _ = c[:]
 print(c)
 d = c.eval()
 np.testing.assert_allclose(c[:], a[:] + b[:])
-
 np.testing.assert_allclose(d[:], a[:] + b[:])
