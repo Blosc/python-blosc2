7ea42d27d945f24da064aac0a42d887c82b6ef83
Francesc Alted <francesc@blosc.org>
2024-06-05 20:37:28 +0200

    More fixes for C2Array example

--- examples/ndarray/c2array_expr.py
+++ examples/ndarray/c2array_expr.py
@@ -5,12 +5,17 @@ import numpy as np
 import blosc2
 
 host = "https://demo.caterva2.net/"
-# host = 'localhost:8002'
-# root = 'example'
-# name = 'dir1/ds-2d.b2nd'
-
 root = "b2tests"
 dir = "expr/"
+
+# For a Caterva2 server running locally, use:
+# host = 'localhost:8002'
+
+# The root of the datasets
+root = "b2tests"
+# The directory inside root where the datasets are stored
+dir = "expr/"
+
 name1 = "ds-0-10-linspace-float64-(True, True)-a1-(60, 60)d.b2nd"
 name2 = "ds-0-10-linspace-float64-(True, True)-a2-(60, 60)d.b2nd"
 path1 = pathlib.Path(f"{root}/{dir + name1}").as_posix()
@@ -18,11 +23,14 @@ path2 = pathlib.Path(f"{root}/{dir + name2}").as_posix()
 
 a = blosc2.C2Array(path1, host)
 b = blosc2.C2Array(path2, host)
-print(a[10:20, 10:20])
+
+# Evaluate only a slice of the expression
 c = a + b
 print(type(c))
-_ = c[:]
-print(c)
-d = c.eval()
+print(c[10:20, 10:20])
+
 np.testing.assert_allclose(c[:], a[:] + b[:])
-np.testing.assert_allclose(d[:], a[:] + b[:])
+
+# Get an NDArray instance instead of a NumPy array
+ndarr = c.eval()
+np.testing.assert_allclose(ndarr[:], a[:] + b[:])
