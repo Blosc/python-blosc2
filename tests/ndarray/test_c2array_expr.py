72a41fe9cd7f6cfbf6b87938ebbd2b1e118e11b7
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2024-06-10 14:01:27 +0200

    Tests: Use auth cookie context manager in C2Array expressions tests

--- tests/ndarray/test_c2array_expr.py
+++ tests/ndarray/test_c2array_expr.py
@@ -34,19 +34,21 @@ DIR = "expr/"
     ]
 )
 def auth_cookie(request):
-    return request.param
+    cookie = request.param
+    with blosc2.c2array.subscriber_auth_cookie(cookie):
+        yield cookie
 
 
-def get_arrays(shape, chunks_blocks, auth_cookie):
+def get_arrays(shape, chunks_blocks):
     dtype = np.float64
     nelems = np.prod(shape)
     na1 = np.linspace(0, 10, nelems, dtype=dtype).reshape(shape)
     urlpath = f"ds-0-10-linspace-{dtype.__name__}-{chunks_blocks}-a1-{shape}d.b2nd"
     path = pathlib.Path(f"{ROOT}/{DIR + urlpath}").as_posix()
-    a1 = blosc2.C2Array(path, urlbase=URLBASE, auth_cookie=auth_cookie)
+    a1 = blosc2.C2Array(path, urlbase=URLBASE)
     urlpath = f"ds-0-10-linspace-{dtype.__name__}-{chunks_blocks}-a2-{shape}d.b2nd"
     path = pathlib.Path(f"{ROOT}/{DIR + urlpath}").as_posix()
-    a2 = blosc2.C2Array(path, urlbase=URLBASE, auth_cookie=auth_cookie)
+    a2 = blosc2.C2Array(path, urlbase=URLBASE)
     # Let other operands be local, on-disk NDArray copies
     urlpath = f"ds-0-10-linspace-{dtype.__name__}-{chunks_blocks}-a3-{shape}d.b2nd"
     a3 = blosc2.asarray(a2, urlpath=urlpath, mode="w")
@@ -70,7 +72,7 @@ def get_arrays(shape, chunks_blocks, auth_cookie):
 )
 def test_simple(chunks_blocks, auth_cookie):
     shape = (60, 60)
-    a1, a2, a3, a4, na1, na2, na3, na4 = get_arrays(shape, chunks_blocks, auth_cookie)
+    a1, a2, a3, a4, na1, na2, na3, na4 = get_arrays(shape, chunks_blocks)
 
     # Slice
     sl = slice(10)
@@ -87,7 +89,7 @@ def test_simple(chunks_blocks, auth_cookie):
 def test_simple_getitem(auth_cookie):
     shape = (NITEMS_SMALL,)
     chunks_blocks = "default"
-    a1, a2, a3, a4, na1, na2, na3, na4 = get_arrays(shape, chunks_blocks, auth_cookie)
+    a1, a2, a3, a4, na1, na2, na3, na4 = get_arrays(shape, chunks_blocks)
     expr = a1 + a2 - a3 * a4
     nres = ne.evaluate("na1 + na2 - na3 * na4")
 
@@ -110,7 +112,7 @@ def test_simple_getitem(auth_cookie):
 )
 def test_ixxx(chunks_blocks, auth_cookie):
     shape = (60, 60)
-    a1, a2, a3, a4, na1, na2, na3, na4 = get_arrays(shape, chunks_blocks, auth_cookie)
+    a1, a2, a3, a4, na1, na2, na3, na4 = get_arrays(shape, chunks_blocks)
     expr = a1**3 + a2**2 + a3**3 - a4 + 3
     expr += 5  # __iadd__
     expr /= 7  # __itruediv__
@@ -123,7 +125,7 @@ def test_ixxx(chunks_blocks, auth_cookie):
 def test_complex(auth_cookie):
     shape = (NITEMS_SMALL,)
     chunks_blocks = "default"
-    a1, a2, a3, a4, na1, na2, na3, na4 = get_arrays(shape, chunks_blocks, auth_cookie)
+    a1, a2, a3, a4, na1, na2, na3, na4 = get_arrays(shape, chunks_blocks)
     expr = blosc2.tan(a1) * blosc2.sin(a2) + (blosc2.sqrt(a4) * 2)
     expr += 2
     nres = ne.evaluate("tan(na1) * sin(na2) + (sqrt(na4) * 2) + 2")
@@ -151,7 +153,7 @@ def test_complex(auth_cookie):
 )
 def test_mix_operands(chunks_blocks, auth_cookie):
     shape = (60, 60)
-    a1, a2, a3, a4, na1, na2, na3, na4 = get_arrays(shape, chunks_blocks, auth_cookie)
+    a1, a2, a3, a4, na1, na2, na3, na4 = get_arrays(shape, chunks_blocks)
     b1 = blosc2.asarray(na1, chunks=a1.chunks, blocks=a1.blocks)
     b3 = blosc2.asarray(na3, chunks=a3.chunks, blocks=a3.blocks)
 
@@ -188,7 +190,7 @@ def test_mix_operands(chunks_blocks, auth_cookie):
 def test_save(auth_cookie):
     shape = (60, 60)
     tol = 1e-17
-    a1, a2, a3, a4, na1, na2, na3, na4 = get_arrays(shape, (False, True), auth_cookie)
+    a1, a2, a3, a4, na1, na2, na3, na4 = get_arrays(shape, (False, True))
 
     expr = a1 * a2 + a3 - a4 * 3
     nres = ne.evaluate("na1 * na2 + na3 - na4 * 3")
@@ -236,10 +238,10 @@ def broadcast_fixture(broadcast_shape, auth_cookie):
     na2 = np.linspace(1, 2, np.prod(shape2), dtype=dtype).reshape(shape2)
     urlpath = f"ds-0-1-linspace-{dtype.__name__}-b1-{shape1}d.b2nd"
     path = pathlib.Path(f"{ROOT}/{DIR + urlpath}").as_posix()
-    b1 = blosc2.C2Array(path, urlbase=URLBASE, auth_cookie=auth_cookie)
+    b1 = blosc2.C2Array(path, urlbase=URLBASE)
     urlpath = f"ds-1-2-linspace-{dtype.__name__}-b2-{shape2}d.b2nd"
     path = pathlib.Path(f"{ROOT}/{DIR + urlpath}").as_posix()
-    b2 = blosc2.C2Array(path, urlbase=URLBASE, auth_cookie=auth_cookie)
+    b2 = blosc2.C2Array(path, urlbase=URLBASE)
 
     return b1, b2, na1, na2
 
