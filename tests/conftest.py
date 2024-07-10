a7e6051c62ab86f28104ad2767435486557024ff
Francesc Alted <francesc@blosc.org>
2024-07-10 14:59:57 +0200

    During testing, honor BLOSC_C2URLBASE envvar

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,10 +5,10 @@
 # This source code is licensed under a BSD-style license (found in the
 # LICENSE file in the root directory of this source tree)
 #######################################################################
+import os
 import pytest
 
 import blosc2
-
 
 # This still needs to pass the '-s' flag to pytest to see the output but anyways
 @pytest.fixture(scope="session", autouse=True)
@@ -22,7 +22,8 @@ def _setup_session():
 def c2sub_context():
     # You may use the URL and credentials for an already existing user
     # in a different Caterva2 subscriber.
-    c2params = dict(urlbase="https://demo.caterva2.net/",
+    urlbase = os.environ.get("BLOSC_C2URLBASE", "https://demo.caterva2.net/")
+    c2params = dict(urlbase=urlbase,
                     username=None, password=None)
     with blosc2.c2context(**c2params):
         yield c2params
