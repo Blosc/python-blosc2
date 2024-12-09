63ce4caabe8d15aaac8cf8719e1b8349cdccd0c7
Francesc Alted <francesc@blosc.org>
2024-12-09 08:31:58 +0100

    Trying with USE_SYSTEM_BLOSC2

---
+++ generate_version.py
@@ -0,0 +1,14 @@
+# generate_version.py
+
+import tomllib as toml
+
+# Read the pyproject.toml file
+with open("pyproject.toml", "rb") as f:
+    pyproject = toml.load(f)
+
+# Extract the version
+version = pyproject["project"]["version"]
+
+# Write the version to blosc2/_version.py
+with open("src/blosc2/_version.py", "w") as f:
+    f.write(f'__version__ = "{version}"\n')
