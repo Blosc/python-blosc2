de7b7b9a971d692568a298bf51d9e24d53e64610
Francesc Alted <francesc@blosc.org>
2025-02-19 14:17:33 +0100

    Add tests for large itemsizes for zeros and nans too

--- tests/ndarray/test_nans.py
+++ tests/ndarray/test_nans.py
@@ -27,3 +27,18 @@ def test_nans_simple(shape, dtype):
 
     b = np.full(shape=shape, fill_value=np.nan, dtype=a.dtype)
     np.testing.assert_allclose(a[...], b)
+
+
+@pytest.mark.parametrize("asarray", [True, False])
+@pytest.mark.parametrize("typesize", [1, 3, 255, 256, 257, 256 * 256])
+@pytest.mark.parametrize("shape", [(1,), (3,), (10,), (2*10,)])
+def test_large_typesize(shape, typesize, asarray):
+    dtype = np.dtype([("f_001", "f8", (typesize,)), ("f_002", "f4", (typesize,))])
+    a = np.full(shape, np.nan, dtype=dtype)
+    if asarray:
+        b = blosc2.asarray(a)
+    else:
+        # b = blosc2.nans(shape, dtype=dtype)  # TODO: this is not working; perhaps deprecate blosc2.nans()?
+        b = blosc2.full(shape, np.nan, dtype=dtype)
+    for field in dtype.fields:
+        np.testing.assert_allclose(b[field], a[field], equal_nan=True)
