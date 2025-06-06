9e6b7e2bef135e80273c778d44dd525b129a55e3
Luke Shaw <luke.shaw@ironarray.io>
2025-06-06 12:58:25 +0200

    Reformatting example for b2-hdf5

--- examples/blosc2_hdf5_compression.py
+++ examples/blosc2_hdf5_compression.py
@@ -1,4 +1,3 @@
-
 #######################################################################
 # Copyright (c) 2019-present, Blosc Development Team <blosc@blosc.org>
 # All rights reserved.
@@ -8,18 +7,19 @@
 #######################################################################
 
 # This shows how to convert a generic .h5 file to a custom blosc2-compressed .h5 file
-# The blosc2 plugin in hdf5plugin doesn't support custom block shapes, and so one 
+# The blosc2 plugin in hdf5plugin doesn't support custom block shapes, and so one
 # has to go a different route for more bespoke compression
 
 import os
-import blosc2
+
 import h5py
 import hdf5plugin
-import numpy as np
+
+import blosc2
 
 clevel = 5  # compression level, e.g., 0-9, where 0 is no compression and 9 is maximum compression
 fname_in = "kevlar.h5"  # input file with the kevlar dataset
-fname_out = f"kevlar-blosc2.h5"
+fname_out = "kevlar-blosc2.h5"
 if not os.path.exists(fname_in):
     raise FileNotFoundError(
         f"Input file {fname_in} does not exist\n"
@@ -47,22 +47,24 @@ with h5py.File(fname_in, "r") as fr:
 # Example 2
 # For other codecs (e.g grok) or for more custom compression such as with user-defined block shapes, one
 # has to use a more involved route
-blocks = (50,80,80)
-chunks = (200,240,240)
+blocks = (50, 80, 80)
+chunks = (200, 240, 240)
 cparams = {
     "codec": blosc2.Codec.LZ4,
     "filters": [blosc2.Filter.BITSHUFFLE],
     "splitmode": blosc2.SplitMode.NEVER_SPLIT,
-    "clevel": clevel
+    "clevel": clevel,
 }
 
-if os.path.exists("dset.b2nd"): # don't reload dset to blosc2 if already done so once
+if os.path.exists("dset.b2nd"):  # don't reload dset to blosc2 if already done so once
     b2im = blosc2.open(urlpath="dset.b2nd", mode="r")
     s, d = b2im.shape, b2im.dtype
 else:
-    with h5py.File(fname_in, "r") as fr: # load file and process to blosc2 array
+    with h5py.File(fname_in, "r") as fr:  # load file and process to blosc2 array
         dset = fr["/entry/data/data"][:]
-        b2im = blosc2.asarray(dset, chunks=chunks, blocks=blocks, cparams=cparams, urlpath="dset.b2nd", mode="w")
+        b2im = blosc2.asarray(
+            dset, chunks=chunks, blocks=blocks, cparams=cparams, urlpath="dset.b2nd", mode="w"
+        )
         d = dset.dtype
         del dset
 
@@ -80,9 +82,14 @@ with h5py.File(fname_out, "w") as fw:
     # Write individual blosc2 chunks directly to hdf5
     # hdf5 requires a cframe, which is only available via blosc2 schunks (not chunks)
     for info in b2im.iterchunks_info():
-        ncoords = tuple(n*chunks[i] for i,n in enumerate(info.coords))
-        aux = blosc2.empty(shape=b2im.chunks, chunks=b2im.chunks, blocks=b2im.blocks,
-                           dtype=b2im.dtype) # very cheap memory allocation
-        aux.schunk.insert_chunk(0, b2im.get_chunk(info.nchunk)) # insert chunk into blosc2 array so we have schunk wrapper (no decompression required)
-        dset_out.id.write_direct_chunk(ncoords, aux.schunk.to_cframe()) # convert schunk to cframe and write to hdf5
+        ncoords = tuple(n * chunks[i] for i, n in enumerate(info.coords))
+        aux = blosc2.empty(
+            shape=b2im.chunks, chunks=b2im.chunks, blocks=b2im.blocks, dtype=b2im.dtype
+        )  # very cheap memory allocation
+        aux.schunk.insert_chunk(
+            0, b2im.get_chunk(info.nchunk)
+        )  # insert chunk into blosc2 array so we have schunk wrapper (no decompression required)
+        dset_out.id.write_direct_chunk(
+            ncoords, aux.schunk.to_cframe()
+        )  # convert schunk to cframe and write to hdf5
     print("Successfully compressed file with custom parameters")
