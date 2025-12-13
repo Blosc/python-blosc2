3bf54c956077a3638c9b5c7e270883aef8a75c41
Francesc Alted <francesc@blosc.org>
2025-12-13 11:11:32 +0100

    Add a comparsion against numexpr

--- bench/ndarray/expr-blocked-eval.py
+++ bench/ndarray/expr-blocked-eval.py
@@ -1,13 +1,15 @@
 from time import time
 import blosc2
 import numpy as np
+import numexpr as ne
 
 N = 10_000
 dtype= np.float32
+cparams = blosc2.CParams(codec=blosc2.Codec.LZ4, clevel=5)
 
 t0 = time()
 #a = blosc2.ones((N, N), dtype=dtype)
-a = blosc2.arange(np.prod((N, N)), shape=(N, N), dtype=dtype)
+a = blosc2.arange(np.prod((N, N)), shape=(N, N), dtype=dtype, cparams=cparams)
 print(f"Time to create data: {(time() - t0) * 1000 :.4f} ms")
 t0 = time()
 b = a.copy()
@@ -15,8 +17,15 @@ c = a.copy()
 print(f"Time to copy data: {(time() - t0) * 1000 :.4f} ms")
 
 t0 = time()
-res = ((a + b) * c).compute()
+res = ((a + b) * c).compute(cparams=cparams)
 print(f"Time to evaluate: {(time() - t0) * 1000 :.4f} ms")
 # print(res.info)
 
-np.testing.assert_allclose(res, (a[:] + b[:]) * c[:])
+na = a[:]
+nb = b[:]
+nc = c[:]
+np.testing.assert_allclose(res, (na + nb) * nc)
+
+t0 = time()
+res = ne.evaluate("(na + nb) * nc")
+print(f"Time to evaluate with NumExpr: {(time() - t0) * 1000 :.4f} ms")
