655efeccb6d88bf79de5d518a092d5b3dce0a0dc
Francesc Alted <francesc@blosc.org>
2025-12-17 17:04:10 +0100

    Add a comparison against NumPy

--- bench/ndarray/expr-blocked-eval.py
+++ bench/ndarray/expr-blocked-eval.py
@@ -5,11 +5,12 @@ import numexpr as ne
 
 N = 10_000
 dtype= np.float32
-cparams = blosc2.CParams(codec=blosc2.Codec.LZ4, clevel=5)
+cparams = blosc2.CParams(codec=blosc2.Codec.BLOSCLZ, clevel=1)
 
 t0 = time()
 #a = blosc2.ones((N, N), dtype=dtype)
 a = blosc2.arange(np.prod((N, N)), shape=(N, N), dtype=dtype, cparams=cparams)
+# a = blosc2.linspace(0., 1., np.prod((N, N)), shape=(N, N), dtype=dtype, cparams=cparams)
 print(f"Time to create data: {(time() - t0) * 1000 :.4f} ms")
 t0 = time()
 b = a.copy()
@@ -29,3 +30,7 @@ np.testing.assert_allclose(res, (na + nb) * nc)
 t0 = time()
 res = ne.evaluate("(na + nb) * nc")
 print(f"Time to evaluate with NumExpr: {(time() - t0) * 1000 :.4f} ms")
+
+t0 = time()
+res = na + nb * nc
+print(f"Time to evaluate with NumPy: {(time() - t0) * 1000 :.4f} ms")
